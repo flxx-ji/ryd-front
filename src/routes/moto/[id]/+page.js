228a4378fd0a2db3c1c4a76936da442495d200a1
Jean-marc Bastareaud <jean-marcbastareaud@FlxxiBook.local>
2025-06-04 03:56:02 +0200

    modification de moto[id]+page.js , ajout de trim pour enlever toutes lettres invisibles espaces

--- src/routes/moto/[id]/+page.js
+++ src/routes/moto/[id]/+page.js
@@ -3,10 +3,10 @@ export const prerender = false;
 
 export async function load({ params, fetch }) {
   const apiUrl = PUBLIC_API_URL;
+  const id = params.id.trim(); // ✨ nettoyage
 
-  const res = await fetch(`${apiUrl}/api/motos/${params.id}`);
+  const res = await fetch(`${apiUrl}/api/motos/${id}`);
 
-  // 🛑 S'il ne trouve pas la moto, on renvoie une erreur claire à Svelte
   if (res.status === 404) {
     return {
       status: 404,
@@ -14,8 +14,8 @@ export async function load({ params, fetch }) {
     };
   }
 
-  // 🛑 Si c’est une autre erreur
   if (!res.ok) {
+    console.error('Erreur API:', await res.text());
     return {
       status: 500,
       error: new Error("Erreur serveur")
