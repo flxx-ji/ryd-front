155ddcbd6e9f5cea841688d164e1e9c3000b8ab5
Jean-marc Bastareaud <cinelli2180@gmail.com>
2026-01-08 11:02:59 +0100

    fix:dans le catalogue stabilisation du chargement du ssr et suppression des doublons client fetch

--- src/routes/catalogue/+page.js
+++ src/routes/catalogue/+page.js
@@ -1,8 +1,19 @@
 // src/routes/catalogue/+page.js
-export const ssr = false;
+import { PUBLIC_API_URL2 } from '$env/static/public';
+import { error } from '@sveltejs/kit';
+
 export const prerender = false;
 
-export function load() {
-  // on laisse le client gérer le fetch (onMount dans +page.svelte)
-  return { motos: [] };
+export async function load({ fetch }) {
+  const baseURL = PUBLIC_API_URL2;
+
+  const res = await fetch(`${baseURL}/api/motos`);
+  if (!res.ok) {
+    throw error(res.status, 'Impossible de récupérer les motos');
+  }
+
+  // ✅ IMPORTANT : on renvoie une PROMISE (pas de await ici)
+  return {
+    motos: res.json()
+  };
 }
