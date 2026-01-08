f8d062038d9730cf168b4e8cba56e17e15edaf0f
Jean-marc Bastareaud <cinelli2180@gmail.com>
2026-01-08 12:02:13 +0100

    simplificaation de catalogue page.js et page.svelte

--- src/routes/catalogue/+page.js
+++ src/routes/catalogue/+page.js
@@ -1,19 +1,15 @@
-// src/routes/catalogue/+page.js
 import { PUBLIC_API_URL2 } from '$env/static/public';
-import { error } from '@sveltejs/kit';
 
 export const prerender = false;
 
 export async function load({ fetch }) {
   const baseURL = PUBLIC_API_URL2;
-
   const res = await fetch(`${baseURL}/api/motos`);
+
   if (!res.ok) {
-    throw error(res.status, 'Impossible de récupérer les motos');
+    throw new Error(`Erreur API motos: ${res.status}`);
   }
 
-  // ✅ IMPORTANT : on renvoie une PROMISE (pas de await ici)
-  return {
-    motos: res.json()
-  };
+  const motos = await res.json();
+  return { motos };
 }
