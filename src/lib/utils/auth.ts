95e3e9a90a48097e6ecbf383684482e2a0e0a1b8
Jean-marc Bastareaud <cinelli2180@gmail.com>
2026-01-22 13:49:25 +0100

    suppression du ficher auth.ts

--- src/lib/utils/auth.ts
+++
@@ -1,4 +0,0 @@
-// 📦 Centralise les accès au token d'admin
-export const getAdminToken = () => localStorage.getItem('adminToken');
-export const setAdminToken = (token: string) => localStorage.setItem('adminToken', token);
-export const removeAdminToken = () => localStorage.removeItem('adminToken');
